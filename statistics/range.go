293c23cabbb54a9589074236ef41320f81ab8a55
weakinyi <akinyiwendy46@gmail.com>
2024-10-14 19:01:41 +0300

    fix: add some regression code

--- statistics/range.go
+++ statistics/range.go
@@ -1,13 +1,25 @@
 package statistics
 
 func Range(numbers []float64) (int, int) {
-        avg := Average(numbers)
-        stdDev := Deviation(numbers)
+	x := make([]float64, len(numbers))
+	for i := range x {
+		x[i] = float64(i)
+	}
 
-        // Adjust factor as needed based on your data
-        factor := 2.57
+	xMean := Average(x)
+	yMean := Average(numbers)
+	slope, intercept := Regression(x, numbers, xMean, yMean)
 
-        lower := int(avg - factor*stdDev)
-        upper := int(avg + factor*stdDev)
-        return lower, upper
+	nextX := float64(len(numbers))
+	predictedNext := slope*nextX + intercept
+
+	// avg := Average(numbers)
+	stdDev := Deviation(numbers)
+
+	// Adjust factor as needed based on your data
+	factor := 2.57
+
+	lower := int(predictedNext - factor*stdDev)
+	upper := int(predictedNext + factor*stdDev)
+	return lower, upper
 }
