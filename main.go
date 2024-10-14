5feb849798cb9c8c8ae78a4c555d5c0b3732e714
weakinyi <akinyiwendy46@gmail.com>
2024-10-14 19:07:05 +0300

    fix: remove unecessary lines

--- main.go
+++ main.go
@@ -3,17 +3,15 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"guess-it/statistics"
 	"log"
 	"os"
 	"strconv"
-
-	"guess-it/statistics"
 )
 
 func main() {
 	numbers := []float64{}
 
-	inputSize := 1
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
@@ -25,43 +23,9 @@ func main() {
 		}
 		numbers = append(numbers, numbs)
 
-		if len(numbers) == 1 {
-			// We need at least two data points to start making predictions
-			fmt.Println("Please provide more numbers.")
-			continue
+		if len(numbers) > 1 {
+			lower, upper := statistics.Range(numbers)
+			fmt.Printf("%d %d\n", lower, upper)
 		}
-
-		allSame := true
-		firstValue := numbers[0]
-		for _, value := range numbers {
-			if value != firstValue {
-				allSame = false
-				break
-			}
-		}
-
-		if allSame {
-			fmt.Println("All values in the dataset are identical")
-			return
-		}
-
-		// Perform linear regression and prediction
-		x := make([]float64, len(numbers))
-		for i := range x {
-			x[i] = float64(i)
-		}
-
-		xMean := statistics.Average(x)
-		yMean := statistics.Average(numbers)
-		slope, intercept := statistics.Regression(x, numbers, xMean, yMean)
-
-		// Predict the next number
-		nextX := float64(inputSize)
-		predictedNext := slope*nextX + intercept
-
-		// Calculate the range for the next number
-		lower, upper := statistics.Range(numbers)
-
-		fmt.Printf("%d %d %.2f\n", lower, upper, predictedNext)
 	}
 }
